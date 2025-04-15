48badeda429be357387386cc0ae3ac1faa7ee5f0
Adérito Marrime <153687419+AderitoMarrime@users.noreply.github.com>
2025-04-15 15:11:45 +0200

    solve first step of exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(userString, number) {
+    let result = '';
 
+    for(let i=0; i<number; i++) {
+        result += userString; 
+    }
+
+    return result;
 };
+
+repeatString('aderito', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
