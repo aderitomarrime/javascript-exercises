e939ee6e5560b145dffe99657244c257e3886be1
Adérito Marrime <153687419+AderitoMarrime@users.noreply.github.com>
2025-04-16 09:51:51 +0200

    make a funtion to reverse string or phrase

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(wordPhrase) {
 
+    let splitedString = wordPhrase.split("");
+    let reversedString = splitedString.reverse();
+    let result = reversedString.join("");
+    return result;
+    
 };
+
+reverseString("Ola mundo");
 
 // Do not edit below this line
 module.exports = reverseString;
