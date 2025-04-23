790b37ec3b01550c10034bc66af0f989cedf7e4a
Adérito Marrime <153687419+AderitoMarrime@users.noreply.github.com>
2025-04-23 11:34:26 +0200

    resolve exercise number 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...args) {
+    return myArray.filter(x => !args.includes(x))
 };
 
+removeFromArray([1,2,3,4], 2,3)
 // Do not edit below this line
 module.exports = removeFromArray;
