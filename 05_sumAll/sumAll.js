c4bd62ca8163e08cac4ecfb5712e970331b34358
Adérito Marrime <153687419+AderitoMarrime@users.noreply.github.com>
2025-04-23 12:33:29 +0200

    make sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, lastNumber) {
+    let result = 0;
+    if (firstNumber < 0 || lastNumber < 0) {
+        return "ERROR"
+    } else if (!Number.isInteger(firstNumber) || !Number.isInteger(lastNumber)) {
+        return "ERROR"
+    } else if (firstNumber === NaN || lastNumber === NaN) {
+        return "Error"
+    }else {
+        if (lastNumber < firstNumber) {
+            for (lastNumber; lastNumber <= firstNumber; lastNumber++) {
+                result += lastNumber
+            }
+        } else {
+            for (firstNumber; firstNumber <= lastNumber; firstNumber++) {
+                result += firstNumber
+            }
+        }
+    }
+    return result
 };
+
+sumAll(2,400)
 
 // Do not edit below this line
 module.exports = sumAll;
