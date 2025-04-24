ff614bc2677529c68d300e042adaab800f19c0af
Adérito Marrime <153687419+AderitoMarrime@users.noreply.github.com>
2025-04-24 13:55:57 +0200

    make a function to identify leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,27 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let result = null;
+    let result1 = Number.isInteger(year / 4)
+    let result2 = Number.isInteger(year / 100)
+    let result3 = Number.isInteger(year / 400)
 
+    if(result1 && !result2 && !result3) {
+        result = true;
+    } else if (!result1 && !result2 && !result3) {
+        result = false
+    } else if (result1 && !result2 && !result3) {
+        result = true
+    } else if (result1 && result2 && !result3) {
+        result = false;
+    } else if (result1 && result2 && result3) {
+        result = true;
+    } else if (result1 && result2 && !result3) {
+        result = false;
+    }
+
+    return result
 };
+
+leapYears(1997)
 
 // Do not edit below this line
 module.exports = leapYears;
