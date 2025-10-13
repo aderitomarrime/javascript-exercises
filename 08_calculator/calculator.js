7352f59445b8d9994d3a42c2be74c0378cd775ac
Adérito Marrime <153687419+aderitomarrime@users.noreply.github.com>
2025-10-13 15:05:36 +0200

    solve the calculator exercise I created some functions with the most frequent operations of a calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+	return number1 + number2;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	return number1 - number2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((accumulator, current) => {
+    return accumulator + current;
+  },0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((accumulator, current) => {
+    return accumulator * current;
+  },1)
 };
 
-const power = function() {
-	
+const power = function(number1, number2) {
+	return number1 ** number2;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let factorial = 1;
+  if (number > 1) {
+    for(let c = number; c > 1; c--) {
+      factorial = number
+      number *= (c-1);
+    }
+    return number
+  }
+  return factorial
 };
 
 // Do not edit below this line
