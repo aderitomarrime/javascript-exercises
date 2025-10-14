fe400b7e1b533990950c6adff280a61a0c3c4e40
Adérito Marrime <153687419+aderitomarrime@users.noreply.github.com>
2025-10-14 19:35:32 +0200

    Resolve fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
 
+    let a = 1;
+    let b = 1;
+    let result = undefined;
+
+    let fibonacciArray = [0,1,1]
+
+    if(number < 0) {
+        return "OOPS"
+    } else {
+        for (let c = number; c > 0; c-- ) {
+            result = a + b;
+            a = b
+            b = result
+    
+            fibonacciArray.push(result)
+        }
+    
+        return fibonacciArray[number];
+    }
+   
 };
 
 // Do not edit below this line
