fb3d1b37b85f4fc60830ed975b8b68633dd199cb
Adérito Marrime <153687419+aderitomarrime@users.noreply.github.com>
2025-10-14 17:54:21 +0200

    Resolve the palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,93 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    function isPunctuation(item) {
+        switch(item) {
+            case '!':
+                return true;
+            break;
+            case '?':
+                return true;
+            break;
+            case '.':
+                return true;
+            break;
+            case ':':
+                return true;
+            break;
+            case ';':
+                return true;
+            break;
+            case ',':
+                return true;
+            break;
+            case '-':
+                return true;
+            break;
+            case '.':
+                return true;
+            break;
+            case ' ':
+                return true;
+            break;
+            default:
+                return false
+            break;
+        }
+    }
+
+    let arrayReversed = null;
+    let arrayToWordReversed = null;
+
+    word = word.toLowerCase()
+
+    let wordToAraay = word.split('');
+
+    let condition = wordToAraay.some(isPunctuation)
+
+    if (condition) {
+
+        let index = null;
+        let punctuation = ['?','!',',','.','-',';',':',' '];
+
+        let continueLoop = false;
+
+        for(let key of punctuation) {
+            index = wordToAraay.indexOf(key)
+            if (index !== -1) {
+                continueLoop = true
+
+                while(continueLoop) {
+                    index = wordToAraay.indexOf(key)
+
+                    if (index == -1) {
+                        continueLoop = false;
+                    } else {
+                        wordToAraay.splice(index,1)
+                    }
+                }
+                
+            }
+        }
+        
+        word = wordToAraay.join('')
+        arrayReversed = wordToAraay.reverse();
+        arrayToWordReversed = arrayReversed.join('')
+        
+    } else {
+        arrayReversed = wordToAraay.reverse();
+        arrayToWordReversed = arrayReversed.join('')
+    }
+
+    let result = false;
+
+    if(word === arrayToWordReversed) {
+        result = true;
+    }
+
+    return result;
 };
+
+palindromes("A car, a man, a maraca.")
 
 // Do not edit below this line
 module.exports = palindromes;
