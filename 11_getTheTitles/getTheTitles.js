c1e9d1f8406705a81483e7dc1360293620104ca8
Adérito Marrime <153687419+aderitomarrime@users.noreply.github.com>
2025-10-14 19:48:27 +0200

    Resolve the getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
+    let arrayBook = []
+    for (let key in books) {
+        arrayBook.push(books[key]["title"])
+    }
+    return arrayBook;
 };
 
 // Do not edit below this line
