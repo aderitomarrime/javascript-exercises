bf32eefb4d7ad49396ad3ec32695f75d20c867da
Adérito Marrime <153687419+aderitomarrime@users.noreply.github.com>
2025-10-16 11:38:27 +0200

    make a function to find the oldest age

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    let peopleAge;
+    let date = new Date();
+    let fullYear = date.getFullYear();
+
+    peopleAge = people.map(person => {
+        if (person["yearOfDeath"] === undefined) {
+            person["yearOfDeath"] = fullYear;
+        }
+        return person["yearOfDeath"] - person["yearOfBirth"];
+    })
+
+    peopleAge.sort((a,b) => a -b);
+    let oldestAge = peopleAge[peopleAge.length-1];
+    
+    let oldestPerson = people.filter(person => {
+        return (person["yearOfDeath"] - person["yearOfBirth"]) == oldestAge;
+    })
+
+    return oldestPerson[0];
 };
 
 // Do not edit below this line
